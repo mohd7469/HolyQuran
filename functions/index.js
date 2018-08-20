3eded696622bd5a250ddc89c748514ab58c7e801
awais786327 <awais3227443@gmail.com>
2018-08-20 12:25:27 +0500

    implemented play and translate module

--- functions/index.js
+++ functions/index.js
@@ -15,27 +15,96 @@ var db = admin.firestore();
 
 /* Import Dialogflow module from the Actions on Google client library */
 const {
-  Suggestions,BrowseCarouselItem, List,NewSurface,MediaObject,BrowseCarousel,
+  SimpleResponse,
+  Suggestions,
   dialogflow,
   BasicCard,
-  Button,
+  MediaObject,
   Image
   } = require('actions-on-google');
 
 /* Instantiate Dialogflow client */
 const app = dialogflow();
 
-let names = [
-  'fatiha', 'para 1', 'para 2', 'para 3', 'para 4',
-  'para 5', 'para 6', 'para 7', 'para 8', 'para 9',
-  'para 10', 'para 11', 'para 12', 'para 13', 'para 14',
-  'kahf', 'para 15', 'para 16', 'para 17', 'para 18',
-  'para 19', 'para 20', 'sajdah', 'para 21', 'para 22',
-  'yaseen', 'para 23', 'para 24', 'dukhan', 'para 25',
-  'para 26', 'fath', 'rehman', 'waqiah', 'para 27',
-  'hashr', 'para 28', 'para 29', 'mulk', 'muzamil',
-  'mudasir', 'para 30', 'ikhlas'
+function getInProcessSSML() {
+  let today = new Date();
+  let date = today.getDate() + '-' + today.getMonth();
+  return `
+        <speak>
+          <break time="500ms"/>
+					<emphasis level="moderate">this section is under development</emphasis>
+          <break time="700ms"/>
+          today is
+          <say-as interpret-as="date" format="dm">${date}</say-as>
+          and i'm trying my best to make it complete
+          <break time="500ms"/>
+					so
+          <break time="500ms"/>
+          i am gonna let you notify soon when this will be done
+          <break time="700ms"/>
+          until you should listen to it
+          <break time="500ms"/>
+          hope you might like it
+        </speak>
+    `;
+}
+
+function getWelcomeSSML() {
+  let msg = [
+    `Hello! Welcome to the Holy Quran, Miracle of Allah and Final Testament to Humankind. Enhance your recitation & spiritual experience with the real feel of Quran anytime anywhere. How can i help ?`,
+    `Hello! Welcome to the Holy Quran, the Last and Final Word of God (Allah) Almighty and a message to all mankind. So its truly says when the Quran is recited, then listen to it and pay attention that you may receive mercy. How can i help ?`
+  ];
+  return msg[Math.floor(Math.random() * msg.length)];
+}
+
+let urduNames = [
+  'الفاتحہ', 'البقرۃ', 'آل عمران', ' ٓالنسا', 'المائدۃ', 'الانعام', 'الاعراف',
+  'الانفال', 'التوبۃ', 'یونس', 'ھود', 'یوسف', 'الرعد', 'ابراھیم', 'الحجر',
+  'النحل', 'الاسرا', 'الکھف', 'مریم', 'طٰہٰ', 'الانبیآ', 'الحج', 'المومنون', 'النور',
+  'الفرقان', 'الشعرآ', 'النمل', 'القصص', 'العنکبوت', 'الروم', 'لقمان', 'السجدۃ',
+  'الاحزاب', 'سبا', 'فاطر', 'یٰس', 'الصافات', 'ص', 'الزمر', 'غافر', 'فصلت',
+  'الشوری', 'الزخرف', 'الدخان', 'الجاثیۃ', 'الاحقاف', 'محمد', 'الفتح', 'الحجرات',
+  'ق', 'الذاریات', 'الطور', 'النجم', 'القمر', 'الرحٰمن', 'الواقعہ', 'الحدید',
+  'المجادلہ', 'الحشر', 'الممتحنۃ', 'الصف', 'الجمعۃ', 'المنافقون', 'التغابن',
+  'الطلاق', 'التحریم', 'الملک', 'القلم', 'الحاقۃ', 'المعارج', 'نوح', 'الجن',
+  'المزمل', 'المدثر', 'القیامۃ', 'الانسان', 'المرثلات', 'النبا', 'النازعات',
+  'عبس', 'التکویر', 'الانفطار', 'المطففین', 'الانشقاق', 'البروج', 'الطارق',
+  'الاعلی', 'الغاشیۃ', 'الفجر', 'البلد', 'الشمس', 'اللیل', 'الضحی', 'الشرح',
+  'التین', 'العلق', 'القدر', 'البینۃ', 'الزلزلۃ', 'العادیات', 'القارعۃ',
+  'التکاثر', 'العصر', 'الھمزۃ', 'الفیل', 'قریش', 'الماعون', 'الکوثر',
+  'الکافرون', 'النصر', 'المسد', 'الاخلاص', 'الفلق', 'الناس'
 ];
+let englishNames = [
+  'fatiha', 'baqarah', 'imran', 'nisa', 'maeda', 'inaam', 'araaf',
+  'anfaal', 'taubah', 'yunus', 'hud', 'yusuf', 'raad', 'ibrahim', 'hijr',
+  'nahl', 'isra', 'kahf', 'maryam', 'taha', 'anbiya', 'hajj', 'mumenoon', 'noor',
+  'furqan', 'shuara', 'naml', 'qasas', 'ankaboot', 'room', 'luqman', 'sajdah',
+  'ahzab', 'saba', 'fatir', 'yaseen', 'saaffat', 'suad', 'zumar', 'ghafir', 'fussilat',
+  'shura', 'zukhruf', 'dukhan', 'jasiya', 'ahqaf', 'muhammad', 'fath', 'hujraat',
+  'qaf', 'zariyat', 'tur', 'najm', 'qamar', 'rehman', 'waqiah', 'hadeed',
+  'mujadila', 'hashr', 'mumtahina', 'saff', 'jumma', 'munafiqoon', 'taghabun',
+  'talaq', 'tahrim', 'mulk', 'qalam', 'haaqqa', 'maarij', 'nooh', 'jinn',
+  'muzamil', 'mudasir', 'qiyama', 'insan', 'mursalat', 'naba', 'naziat',
+  'abasa', 'takwir', 'infitar', 'mutaffifin', 'inshiqaq', 'burooj', 'tariq',
+  'alaa', 'ghashiya', 'fajr', 'balad', 'shams', 'lail', 'zuha', 'nashrah',
+  'tin', 'alaq', 'qadr', 'bayyina', 'zalzala', 'adiyaat', 'qaria',
+  'takasur', 'asr', 'humaza', 'feel', 'quraish', 'maun', 'kausar',
+  'kafiroon', 'nasr', 'masad', 'ikhlas', 'falaq', 'naas'
+];
+
+let names = englishNames;
+
+function shuffleNames(type1, type2) {
+  let index = type1.length;
+  let rnd, tmp1, tmp2;
+  while (index) {
+    rnd = Math.floor(Math.random() * index);
+    index -= 1;
+    tmp1 = type1[index]; tmp2 = type2[index];
+    type1[index] = type1[rnd]; type2[index] = type2[rnd];
+    type1[rnd] = tmp1; type2[rnd] = tmp2;
+  }
+}
 
 function getOpeningRandomMessage(surah) {
   let msg = [
@@ -48,34 +117,63 @@ function getOpeningRandomMessage(surah) {
     `sure, opening surah ${surah}`,
     `ok, there we go`
   ];
-  switch (surah) {
-    case 'last twenty': {
-      return `okay, now opening ${surah} surah of Holy Quran`;
-    }
-    default: {
-      /* Shuffle ang get random message */
-      let text = msg[Math.floor(Math.random() * msg.length)];
-
-      /* if request contain the word "para", then remove the text "surah" from msg */
-      if(surah.indexOf('para') !== -1) {
-        text = text.replace('surah', '');
-      }
-
-      return text;
-    }
-  }
+  /* Shuffle ang get random message */
+  return msg[Math.floor(Math.random() * msg.length)];
 }
 
-app.intent('read surah', (conv, {surah}) => {
+function displaySuggestions() {
+  /* copy first 7 random names for suggestions */
+  let suggestion = names.slice(0, 6);
+  suggestion.push('↺');
+  return new Suggestions(suggestion);
+}
+
+function getRandomMsg(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+app.intent('Default Welcome Intent', (conv) => {
+
+  conv.ask(new SimpleResponse({
+    speech: getWelcomeSSML(),
+    text: `Welcome to the Holy Quran`
+  }));
+
+  let welcomeView = new BasicCard({
+    text: "The Islamic sacred book written down in Arabic, which Muslims to be a revelation from God **(Allah)** as dictated to **Muhammad ﷺ**. **Quran** consists of 114 units of varying lengths, known as ***Surah***. These touch upon all aspects of human existence, including matters of doctrine, social organization, and legislation.",
+    subtitle: 'The recitation',
+    title: 'Al-Qurʾan القرآن',
+    image: new Image({
+      url: 'https://onehdwallpaper.com/wp-content/uploads/2014/11/Quran-HD-Wallpapers-Free-Download-For-Desktop.jpg',
+      alt: 'The Holy Quran'
+    })
+  });
+  conv.add(welcomeView);
+  conv.add(new Suggestions('listen', 'read'));
+
+});
+
+app.intent('read', (conv) => {
+
+  conv.ask(new SimpleResponse({
+    speech: getInProcessSSML(),
+    text: 'This section is under development.'
+  }));
+  conv.add(new Suggestions('listen'));
+
+});
+
+app.intent('listen', (conv, {surah}) => {
+  console.info(conv.user.storage);
 
   /* for checking supported screen device capability */
-  if (!conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT')) {
-    conv.ask(`Sorry, try this on a supported screen device your phone doesn't support rich responses`);
+  if (!conv.surface.capabilities.has('actions.capability.MEDIA_RESPONSE_AUDIO')) {
+    conv.close(`hey, looks like your device doesn't support audio compatibility, you should open it on your mobile phone or other supported device`);
     return;
   }
 
   /* Shuffle quran surah names everytime whenever this intent call */
-  names.sort(() => { return 0.5 - Math.random() });
+  shuffleNames(englishNames, urduNames);
 
   let data = {};
 
@@ -88,108 +186,57 @@ app.intent('read surah', (conv, {surah}) => {
     })
     .then(() => {
       conv.ask(getOpeningRandomMessage(surah));
-
-      let displaySurah = new BasicCard({
-        text: data[surah].text,
-        subtitle: data[surah].alt,
-        //title: 'Title: this is a title',
-        image: new Image(data[surah]),
-        buttons: data[surah].view ? new Button({
-          title: 'Continue reading..',
-          url: data[surah].view
-        }) : null
+      let playAudio = new MediaObject({
+        name: `Surah ${data[surah]['alt']}`,
+        url: data[surah]['audio'],
+        description: data[surah]['subtitle'] ? data[surah]['subtitle'] : null,
+        icon: new Image({
+          url: 'https://pbs.twimg.com/profile_images/677515986828939264/N_vffeWw_400x400.png',
+          alt: data[surah]['alt']
+        })
       });
 
-      /* now copy first five random names for suggestions which we have Shuffle */
-      let suggestion = names.slice(0, 5);
-
-      /* if request is already for last twenty than do not show in suggestion */
-      if(surah !== 'last twenty') suggestion.push('last twenty surah');
-
-      let displaySuggestions = new Suggestions(suggestion);
-
-      conv.add(displaySurah);
-      conv.add(displaySuggestions);
+      conv.add(playAudio);
+      conv.add(displaySuggestions());
       return;
     })
     .catch((err) => {
-      conv.close(`i am sorry for this ${err}`);
       console.log('Error getting documents ', err);
+      conv.close(`i am sorry for this ${err}`);
     });
 
 });
 
-app.intent('test', (conv) => {
+app.intent('playback completed', (conv) => {
+  const mediaStatus = conv.arguments.get('MEDIA_STATUS');
+  let response = 'Hope you enjoyed it.';
+  if (mediaStatus && mediaStatus.status === 'FINISHED') {
+    response = getRandomMsg([
+      'You might also like to listen these.',
+      'You might like these.',
+      'You should also listen to this.',
+      `You'll love to listen these.`,
+      `You'd love to hear these.`
+    ]);
+  }
 
-  //const screenAvailable = conv.available.surfaces.capabilities.has('actions.capability.SCREEN_OUTPUT');
-  //const context = 'Sure, I have some sample images for you.';
-  //const notification = 'Sample Images';
-  //const capabilities = ['actions.capability.SCREEN_OUTPUT'];
-  //if (screenAvailable) {
-  //  conv.ask(new NewSurface({
-  //    context,
-  //    notification,
-  //    capabilities
-  //  }));
-  //} else {
-  //  conv.close("Sorry, you need a screen to see pictures");
-  //}
-
-  //if (!conv.surface.capabilities.has('actions.capability.MEDIA_RESPONSE_AUDIO')) {
-  //  conv.ask('Sorry, this device does not support audio playback.');
-  //  return;
-  //}
-  //conv.ask('Playing Surah Rehman...');
-  //
-  //conv.ask(new MediaObject({
-  //  name: 'Surah Rehman',
-  //  url: 'https://media.blubrry.com/muslim_central_quran/audio1.qurancentral.com/fares-abbad/fares-abbad-055.mp3',
-  //  description: 'This is Surah Rehman',
-  //  icon: new Image({
-  //    url: 'https://freeislamiccalligraphy.com/wp-content/uploads/2014/07/Al-Rahman.jpg',
-  //    alt: 'Ar-Rehman'
-  //  })
-  //}));
-  //conv.add(new Suggestions('Surah Mulk', 'Surah Yaseen', 'Surah Ikhlas', 'Surah Hashr', 'Surah Sajda', 'Surah Tauba'));
-
-  conv.ask('browse items ...');
-  conv.ask(new BrowseCarousel({
-    items: [
-      new BrowseCarouselItem({
-        title: 'Mulk',
-        url: 'https://firebasestorage.googleapis.com/v0/b/holy-quran-e99eb.appspot.com/o/popular%2Fmulk%2Fcover.png?alt=media&token=b23113a9-cc75-4f10-b020-2c40b978e80d',
-        description: 'Description of Surah Mulk Image',
-        image: new Image({
-          url: 'https://images.pexels.com/photos/908283/pexels-photo-908283.jpeg?auto=compress&cs=tinysrgb&h=350',
-          alt: 'Surah Mulk'
-        }),
-        footer: 'Surah Mulk footer'
-      }),
-      new BrowseCarouselItem({
-        title: 'Title of item 2',
-        url: 'https://www.google.com',
-        description: 'Description of item 2',
-        image: new Image({
-          url: 'https://images.pexels.com/photos/908283/pexels-photo-908283.jpeg?auto=compress&cs=tinysrgb&h=350',
-          alt: 'Image text 1'
-        }),
-        footer: 'Item 2 footer'
-      })
-    ]
-  }));
+  conv.ask(response);
+  conv.add(displaySuggestions());
 });
 
-const itemResponses = {
-  'Carousel 1': 'You selected the first item',
-  'Carousel 2': 'You selected the second item'
-};
-
-app.intent('custom intent', (conv, params, option) => {
-  let response = 'You did not select any item';
-  if (option && itemResponses.hasOwnProperty(option)) {
-    response = itemResponses[option];
+app.intent('translate', (conv) => {
+  console.info(conv.user.storage);
+  conv.ask(getRandomMsg(['Okay!', 'Translating...', 'Alright!', 'Translating.']));
+  if(conv.user.storage.language && conv.user.storage.language === 'en') {
+    console.info('setting urdu names');
+    names = urduNames;
+    conv.user.storage.language = 'urdu';
+  } else {
+    console.info('setting english names');
+    names = englishNames;
+    conv.user.storage.language = 'en';
   }
-  conv.ask(response);
+  conv.add(displaySuggestions());
 });
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
